0dbf9ef35ac0a4cb580fcaf9b13268502b02940a
Kevin Lee <kevinwarrenlee@gmail.com>
2010-09-28 21:24:09 -0500

    added devise and cancan to site

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,4 +1,7 @@
 class MealsController < ApplicationController
+  before_filter :authenticate_user!
+  load_and_authorize_resource
+  
   # GET /meals
   # GET /meals.xml
   def index
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100918055940) do
+ActiveRecord::Schema.define(:version => 20100929021636) do
 
   create_table "dressings", :force => true do |t|
     t.string   "description", :null => false
@@ -42,5 +42,25 @@ ActiveRecord::Schema.define(:version => 20100918055940) do
     t.datetime "updated_at",  :null => false
     t.integer  "meal_id",     :null => false
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                               :default => "", :null => false
+    t.string   "encrypted_password",   :limit => 128, :default => "", :null => false
+    t.string   "password_salt",                       :default => "", :null => false
+    t.string   "reset_password_token",                                :null => false
+    t.string   "remember_token",                                      :null => false
+    t.datetime "remember_created_at",                                 :null => false
+    t.integer  "sign_in_count",                       :default => 0,  :null => false
+    t.datetime "current_sign_in_at",                                  :null => false
+    t.datetime "last_sign_in_at",                                     :null => false
+    t.string   "current_sign_in_ip",                                  :null => false
+    t.string   "last_sign_in_ip",                                     :null => false
+    t.datetime "created_at",                                          :null => false
+    t.datetime "updated_at",                                          :null => false
+    t.string   "role",                                                :null => false
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
